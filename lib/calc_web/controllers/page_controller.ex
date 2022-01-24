e655aba95df0e1458d5caa33a0124ca67cd70b9b
Gabriel Aragon <thirtythreebananas@gmail.com>
2022-01-24 13:23:13 -0800

    working

--- lib/calc_web/controllers/page_controller.ex
+++ lib/calc_web/controllers/page_controller.ex
@@ -1,7 +1,17 @@
 defmodule CalcWeb.PageController do
   use CalcWeb, :controller
 
-  def index(conn, _params) do
-    render(conn, "index.html")
+  def index(conn, params) do
+
+    if (params |> is_nil()) do
+      conn
+      |> put_flash(:info, "")
+      |> render("index.html")
+    else
+      result = Operations.operate(params)
+      conn
+      |> render("index.html", result: result, params: params)
+    end
+
   end
 end
